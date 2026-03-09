46bc4315e8e7367ea16b0e3c9ddf2a296f8a009b
Subh Raj Shaw <subhrajshaw1843@gmail.com>
2026-03-09 21:42:05 +0530

    fixed index.js issue

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -10,12 +10,12 @@ const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk' },
     {event: 'clerk/user.created'},
     async ({ event }) => {
-        const { id, first_name, last_name, email_address, image_url } = event.data;
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
         // Yahan pe aap apne database mein user data save kar sakte ho.
         const userData = {
             _id: id,
             name: first_name + ' ' +last_name,
-            email: email_address[0].email_address,
+            email: email_addresses[0].email_address,
             image: image_url
         };
         await User.create(userData);
@@ -24,7 +24,7 @@ const syncUserCreation = inngest.createFunction(
 
 // Inngest function to delete user data from database when user is deleted from clerk
 const syncUserDeletion = inngest.createFunction(
-    {id: 'delet-user-with-clerk' },
+    {id: 'delete-user-with-clerk' },
     {event: 'clerk/user.deleted'},
     async ({ event }) => {
         const { id } = event.data;
